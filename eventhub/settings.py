e9e716693a61f4a126365795eedb58ffc6e536b5
rodriguezemautn <erodriguezrodriguez@alu.frlp.utn.edu.ar>
2025-05-12 19:02:43 -0300

    ADD/ files Experiencia 1: Containerización con Dockerfile

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure--f67ll=2-b2qolla9=1f8mtg@s=l8^y8aj=@ij-0f4)eg@%8(0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
